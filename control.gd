70d34ebe6a33c5ed73596440199d00c7f9a5c18e
brb-fr <tt.brbfr@gmail.com>
2025-08-05 01:16:54 +0300

    v2

--- control.gd
+++ control.gd
@@ -2,6 +2,8 @@ extends ColorRect
 
 var holding := false
 var last_pos :=Vector2i()
+@onready var downloader: HTTPRequest = $Downloader
+@onready var github: HTTPRequest = $GithubGetter
 
 func _on_sensor_button_down() -> void:
 	holding = true
@@ -20,3 +22,18 @@ func _on_x_pressed() -> void:
 
 func _on__pressed() -> void:
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MINIMIZED)
+
+#[b]Downlaoding Parkour...[/b]\nRetrieving necessary packages...
+#[b]Launching Parkour[/b]\nExecuting Parkour.exe
+#[b]Downloading Parkour...[/b]\n0 / 123MB - 1MB/s
+#[b]Downloading Parkour...[/b]\nPreparing...
+
+func _on_play_pressed() -> void:
+	$Anim.play("play")
+	$LOWER/Text.text = "[b]Downloading Parkour...[/b]\nRetrieving necessary packages..."
+	github.request("https://raw.githubusercontent.com/brb-fr/Parkour-Updates/refs/heads/main/mirror")
+	var gh = await github.request_completed
+	if gh[0] == 200:
+		$LOWER/Text.text = "[b]Downloading Parkour...[/b]\nPreparing..."
+		downloader.request(JSON.parse_string(gh[3].get_string_from_utf8().mirror))
+		var dw = dow
